22a057eb529dbb3700ebaa7ad9c71c7b71ef1425
pzachprajana <preethi@prajana.ai>
2025-05-12 21:16:25 -0700

    Update next.config.ts
    Added output: 'export', to deploy Next.js app as a static site to Firebase Hosting,

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Added this line to deploy Next.js app as a static site to Firebase Hosting,
   typescript: {
     ignoreBuildErrors: true,
   },
